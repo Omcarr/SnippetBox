d5f51b0c8eafdc2a2bfc5db9e0d1646465472573
Omkar <omkarnova@gmail.com>
2025-03-14 20:15:48 +0530

    logger dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	// "flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create/", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// addr := flag.String("addr", ":4000", "HTTP network address")
+	// flag.Parse()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":4000" // Default to 4000 if not set
+	}
+
+	//logger creation
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+
+	//injecting logger dependency to the entire app
+	app := &application{
+		logger: logger,
+	}
+
+	// logger.Info("port", port)
+	logger.Info("starting server on", slog.String("port", port))
+	err = http.ListenAndServe(port, app.routes())
+
+	logger.Error(err.Error())
+	os.Exit(1)
+
 }
